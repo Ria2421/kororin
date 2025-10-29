8e494abcee329263c23fc44b2a2ec1ee798f69b3
Rui Enomoto <231013@st.yoshida-g.ac.jp>
2025-10-29 13:24:05 +0900

    InspectorFoldoutインポート

---
+++ src/Kororin.Unity/Assets/02_Enomoto/02_Scripts/HedgehogBase.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class HedgehogBase : MonoBehaviour
+{
+    [SerializeField]
+    protected Animator animator;
+    [SerializeField]
+    protected Avatar ballAvatar;
+    [SerializeField]
+    protected Avatar defaultAvatar;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
