8b441f31583b3cf676468c3794670f7bbc854ef5
Souma Ueno <231116@st.yoshida-g.ac.jp>
2025-11-17 15:59:22 +0900

    ギミック大きさ調整・動き作成

---
+++ src/Kororin.Unity/Assets/01_Ueno/02_Scripts/Gimmick/SwordSpin.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class SwordSpin : MonoBehaviour
+{
+    [SerializeField] float rollY = 15f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rollY = Random.Range(220, 380);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.Rotate(new Vector3(0, rollY, 0) * Time.deltaTime);
+    }
+}
