a5f8637a2c789e3bf7ab2a56952be71e1a384f6a
Souma Ueno <231116@st.yoshida-g.ac.jp>
2025-11-18 12:05:32 +0900

    スライサーのギミック作成

---
+++ src/Kororin.Unity/Assets/01_Ueno/02_Scripts/Gimmick/Sliser.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class Sliser : MonoBehaviour
+{
+    [SerializeField] float rollX;
+
+    // 移動速度 (インスペクターで調整)
+    float speed;
+
+    // Z軸の往復距離 (初期位置から片側への最大距離)
+    [SerializeField] float maxDistance = 1f;
+
+    private float startX,startZ;
+
+    void Start()
+    {
+        // ギミックの基準となるZ座標を記録
+        startZ = transform.localPosition.z;
+        startX = transform.localPosition.x; // X座標を固定
+
+        speed = Random.Range(1.5f, 5);
+    }
+
+    void Update()
+    {
+        // Time.time * speed で、時間に速度を掛けた分だけ値が変化
+        float pingPong = Mathf.PingPong(Time.time * speed, maxDistance * 2f);
+
+        // 範囲を -maxDistance から +maxDistance にシフト
+        float offsetZ = pingPong - maxDistance;
+
+        // 新しいZ座標を計算し、位置を更新
+        transform.localPosition = new Vector3(
+            startX,
+            transform.localPosition.y,
+            startZ + offsetZ
+        );
+
+        gameObject.transform.Rotate(new Vector3(-rollX, 0, 0) * Time.deltaTime);
+    }
+}
