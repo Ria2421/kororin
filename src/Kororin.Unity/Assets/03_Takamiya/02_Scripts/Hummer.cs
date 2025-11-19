1323c23d7e4893db45436f8cfe68afc8d8795f21
yuutotakamiya <231065@st.yoshida-g.ac.jp>
2025-11-19 11:37:20 +0900

    ハンマーの振る角度を修正

--- src/Kororin.Unity/Assets/03_Takamiya/02_Scripts/Hummer.cs
+++ src/Kororin.Unity/Assets/03_Takamiya/02_Scripts/Hummer.cs
@@ -3,19 +3,21 @@ using UnityEngine;
 
 public class Gimmck : GimmickBase
 {
-    [SerializeField] float angle = 45f;        // 振れる角度
-    [SerializeField] float duration = 1.5f;    // 片道の時間
+    [SerializeField] float angle;        // 振れる角度
+    [SerializeField] float duration;    // 片道の時間
+    [SerializeField] bool startFromRight = false;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
 
-        float startAngle = -angle;
+        float startAngle = startFromRight ? -angle : angle;
+        float endAngle = startFromRight ? angle : -angle;
 
         transform.rotation = Quaternion.Euler(0, 0, startAngle);
         // 左右に揺れるアニメーション（無限ループ）
         transform
-            .DORotate(new Vector3(0, 0, angle), duration)
+            .DORotate(new Vector3(0, 0, endAngle), duration)
             .SetEase(Ease.InOutSine)
             .SetLoops(-1, LoopType.Yoyo);
     }
