1c21adcb1b9bb22f1cfd757b1705c7f07d3a72da
Souma Ueno <231116@st.yoshida-g.ac.jp>
2025-11-19 10:05:09 +0900

    プレス機完成

---
+++ src/Kororin.Unity/Assets/01_Ueno/02_Scripts/Gimmick/Press.cs
@@ -0,0 +1,72 @@
+//------------------------------------------
+// プレスギミック [ Press.cs ]
+// Author:Souma Ueno
+//------------------------------------------
+
+using DG.Tweening;
+using UnityEngine;
+
+public class Press : MonoBehaviour
+{
+    [Header("移動時間")]
+    [SerializeField] float moveTime;      // プレスにかかる時間
+
+    [Header("戻る時間")]
+    [SerializeField] float resetMoveTime; // 初期位置に戻る時間
+
+    [Header("停止時間")]
+    [SerializeField] float stopTime;      // 動いた後に止まる時間
+
+    [Header("ループにかかる時間")]
+    [SerializeField] int roopTime;
+
+    // 初期位置保存用
+    float initPosZ;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        initPosZ = transform.localPosition.z;
+
+        StartPressLoop();
+    }
+
+    /// <summary>
+    /// プレスの移動
+    /// </summary>
+    private Tween MovePosZ()
+    {
+        return transform.DOLocalMoveZ(0, moveTime);
+    }
+
+    /// <summary>
+    /// 初期位置に戻る
+    /// </summary>
+    private Tween ResetPosZ()
+    {
+        return transform.DOLocalMoveZ(initPosZ, resetMoveTime);
+    }
+
+    /// <summary>
+    /// プレスのループ
+    /// </summary>
+    private void StartPressLoop()
+    {
+        DOTween.Kill(transform);
+
+        Sequence pressSequence = DOTween.Sequence();
+
+        pressSequence.Append(MovePosZ());
+
+        // 一定時間停止
+        pressSequence.AppendInterval(stopTime);
+
+        pressSequence.Append(ResetPosZ());
+
+        // 一定時間停止
+        pressSequence.AppendInterval(stopTime);
+
+        // ループ
+        pressSequence.SetLoops(-roopTime);
+    }
+}
